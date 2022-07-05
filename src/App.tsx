dab6f6f864ab3b5a1dbb202d3410a5deb400f4bc
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-05 20:47:03 +0200

    installed bootstrap and inserted a standard bootstrap navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import NavigationBar from './Components/navigationBar';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
+        <NavigationBar />
 <h1>hello</h1>
       </header>
 
