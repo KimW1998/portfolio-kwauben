87509fbcb6c60375bbf1e316330308514a81ae1e
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-13 16:08:03 +0200

    created the basics for the skillsboxes

--- src/Components/SkillsBox.tsx
+++ src/Components/SkillsBox.tsx
@@ -1,9 +1,28 @@
+import { Col, Container, ProgressBar, Row } from "react-bootstrap";
+import HTML5 from '../Images/html5.svg'
 
 const SkillsBox = () => {
+    const now = 60;
     return (
         <div>
             
-            <h1 className="test-tekst"> Skill...</h1>
+            <Container>
+                <Row>
+                    <Col>
+                    <img src={HTML5} alt="" />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <h3 className="test-tekst">HTML5</h3>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    <ProgressBar now={now} label={`${now}%`} />;
+                    </Col>
+                </Row>
+            </Container>
            
     </div>
     );
