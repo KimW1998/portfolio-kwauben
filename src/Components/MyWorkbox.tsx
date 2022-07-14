77665dca924291ca4a9d1a301a0943b2cf320d69
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-07-14 15:49:36 +0200

    created a basic layout for myworkbox

--- src/Components/MyWorkbox.tsx
+++ src/Components/MyWorkbox.tsx
@@ -1,20 +1,33 @@
-import { Button, Card } from "react-bootstrap";
+import { Button, Card, Col, Row } from "react-bootstrap";
+import GitHub from '../Images/gitlab.png'
 
-
-const MyWorkBox = (props: { src: string; }) => {
+const MyWorkBox = (props: {
+  GithubLink: string;
+  text: string;
+  title: string; 
+  src: string; 
+  used: string;
+}) => {
     return (
         <div>
             
             <Card className="MyWorkBox" style={{ width: '18rem' }}>
             <Card.Img variant="top" src={props.src} />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+    <Card.Title className="text-left">{props.title}</Card.Title>
+    <Card.Text className="text-left">
+      {props.text}
     </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
+    <Card.Subtitle className="mb-2 text-left">{props.used}</Card.Subtitle>
+    <Row>
+      <Col>
+    <Button variant="primary">Live Demo</Button>
+    </Col>
+    <Col>
+    <a href="https://github.com/KimW1998" target="_blank" rel="noreferrer" ><img src={GitHub} alt="Link to projects github" width={70} /></a>
+    
+    </Col>
+    </Row>
   </Card.Body>
 </Card>
            
